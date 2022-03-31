5c4d4de88eaafeb9a879674477c48c5c1ff77fe0
卢旭昌 <wb-luxc@sdc.icbc.com.cn>
2022-03-31 17:22:08 +0800

    增加  注册页面

---
+++ app/src/main/java/com/luxc/moneymanager/activity/manager/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.luxc.moneymanager.activity.manager;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.luxc.moneymanager.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
