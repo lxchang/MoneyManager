70176e6d2063b4f01c3f754a556973855df7ec3c
卢旭昌 <wb-luxc@sdc.icbc.com.cn>
2022-04-06 17:38:29 +0800

    增加用户列表模糊查询功能

--- app/src/main/java/com/luxc/moneymanager/utils/DaoUtils.java
+++ app/src/main/java/com/luxc/moneymanager/utils/DaoUtils.java
@@ -28,7 +28,7 @@ public class DaoUtils {
      * @return
      */
     public static List<UserBean> querySearchByName(String userName) {
-        return MyApp.getInstance().getDaoSession().getUserBeanDao().queryBuilder().where(UserBeanDao.Properties.PhoneNum.like(userName)).list();
+        return MyApp.getInstance().getDaoSession().getUserBeanDao().queryBuilder().where(UserBeanDao.Properties.Name.like("%"+userName+"%")).list();
     }
 
     /**
